2e41412354b3b5ede7ec508cd1da7b6bd45c027b
Jérôme Respaut <jerome.respaut@fr.clara.net>
2020-07-09 15:38:19 +0200

    Revert "AZ-108: Prevent for_each apply error by keeping the module version with"
    This reverts commit 48fdff731fa5d741740528a8ff353b533662eb8f.

--- r-diagnostics.tf
+++ r-diagnostics.tf
@@ -2,19 +2,17 @@ module "vm-logs" {
   source  = "claranet/vm-logs/azurerm"
   version = "2.2.0"
 
-  location       = var.location
-  location_short = var.location_short
-  client_name    = var.client_name
-  environment    = var.environment
-  stack          = var.stack
+  location    = var.location
+  client_name = var.client_name
+  environment = var.environment
+  stack       = var.stack
 
   resource_group_name = var.resource_group_name
 
   diagnostics_storage_account_name      = var.diagnostics_storage_account_name
   diagnostics_storage_account_sas_token = var.diagnostics_storage_account_sas_token
 
-  vm_id   = azurerm_virtual_machine.vm.id
-  vm_name = azurerm_virtual_machine.vm.name
+  vm_ids = [azurerm_virtual_machine.vm.id]
 
   tags = {
     environment = var.environment
