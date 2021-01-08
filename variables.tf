be7514922d1614745fe3912a7f128543a4ef2e19
frederic peraud <frederic.peraud@fr.clara.net>
2021-01-08 14:50:17 +0100

    update storage data disk variable definition

--- variables.tf
+++ variables.tf
@@ -149,15 +149,17 @@ variable "vm_image" {
 
 variable "storage_data_disk_config" {
   description = <<EOT
-Map to configure data storage disk.
-      name                 = string  
-      create_option        = string
-      disk_size_gb         = string
-      lun                  = string
-      storage_account_type = string
+Map of objects to configure storage data disk(s).
+    disk1 = {
+      name                 = string , 
+      create_option        = string ,
+      disk_size_gb         = string ,
+      lun                  = string ,
+      storage_account_type = string ,
       extra_tags           = map(string)
+    }
 EOT
-  type        = map()
+  type        = map(any)
   default     = {}
 }
 
