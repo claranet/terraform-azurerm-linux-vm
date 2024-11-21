444f7ca00a8ecfee724e99ba8edd159eb6f93829
Jérôme Respaut <jerome.respaut@claranet.com>
2024-11-21 16:21:31 +0100

    docs: update examples

--- examples/aadsshlogin/modules.tf
+++ examples/aadsshlogin/modules.tf
@@ -1,118 +1,3 @@
-module "azure_region" {
-  source  = "claranet/regions/azurerm"
-  version = "x.x.x"
-
-  azure_region = var.azure_region
-}
-
-module "rg" {
-  source  = "claranet/rg/azurerm"
-  version = "x.x.x"
-
-  location    = module.azure_region.location
-  client_name = var.client_name
-  environment = var.environment
-  stack       = var.stack
-}
-
-module "azure_network_vnet" {
-  source  = "claranet/vnet/azurerm"
-  version = "x.x.x"
-
-  environment    = var.environment
-  location       = module.azure_region.location
-  location_short = module.azure_region.location_short
-  client_name    = var.client_name
-  stack          = var.stack
-
-  resource_group_name = module.rg.resource_group_name
-  vnet_cidr           = ["10.10.0.0/16"]
-}
-
-module "azure_network_subnet" {
-  source  = "claranet/subnet/azurerm"
-  version = "x.x.x"
-
-  environment    = var.environment
-  location_short = module.azure_region.location_short
-  client_name    = var.client_name
-  stack          = var.stack
-
-  resource_group_name  = module.rg.resource_group_name
-  virtual_network_name = module.azure_network_vnet.virtual_network_name
-  subnet_cidr_list     = ["10.10.10.0/24"]
-
-  route_table_name = module.azure_network_route_table.route_table_name
-
-  network_security_group_name = module.network_security_group.network_security_group_name
-}
-
-module "network_security_group" {
-  source  = "claranet/nsg/azurerm"
-  version = "x.x.x"
-
-  client_name         = var.client_name
-  environment         = var.environment
-  stack               = var.stack
-  resource_group_name = module.rg.resource_group_name
-  location            = module.azure_region.location
-  location_short      = module.azure_region.location_short
-}
-
-module "azure_network_route_table" {
-  source  = "claranet/route-table/azurerm"
-  version = "x.x.x"
-
-  client_name         = var.client_name
-  environment         = var.environment
-  stack               = var.stack
-  location            = module.azure_region.location
-  location_short      = module.azure_region.location_short
-  resource_group_name = module.rg.resource_group_name
-}
-
-resource "azurerm_availability_set" "vm_avset" {
-  name                = "${var.stack}-${var.client_name}-${module.azure_region.location_short}-${var.environment}-as"
-  location            = module.azure_region.location
-  resource_group_name = module.rg.resource_group_name
-  managed             = true
-}
-
-module "run" {
-  source  = "claranet/run/azurerm"
-  version = "x.x.x"
-
-  client_name         = var.client_name
-  environment         = var.environment
-  stack               = var.stack
-  location            = module.azure_region.location
-  location_short      = module.azure_region.location_short
-  resource_group_name = module.rg.resource_group_name
-
-  monitoring_function_enabled = false
-  vm_monitoring_enabled       = true
-  backup_vm_enabled           = true
-  update_center_enabled       = true
-
-  update_center_periodic_assessment_enabled = true
-  update_center_periodic_assessment_scopes  = [module.rg.resource_group_id]
-  update_center_maintenance_configurations = [
-    {
-      configuration_name = "Donald"
-      start_date_time    = "2021-08-21 04:00"
-      recur_every        = "1Day"
-    },
-    {
-      configuration_name = "Hammer"
-      start_date_time    = "1900-01-01 03:00"
-      recur_every        = "1Week"
-    }
-  ]
-
-  recovery_vault_cross_region_restore_enabled = true
-  vm_backup_daily_policy_retention            = 31
-}
-
 data "azuread_group" "vm_admins_group" {
   display_name = "Virtual Machines Admins"
 }
@@ -130,27 +15,31 @@ module "vm" {
   client_name         = var.client_name
   environment         = var.environment
   stack               = var.stack
-  resource_group_name = module.rg.resource_group_name
+  resource_group_name = module.rg.name
 
-
-  subnet_id      = module.azure_network_subnet.subnet_id
+  subnet = {
+    id = module.subnet.id
+  }
   vm_size        = "Standard_B2s"
-  custom_name    = "app-${var.stack}-${var.client_name}-${module.azure_region.location_short}-${var.environment}-vm"
   admin_username = var.vm_administrator_login
   ssh_public_key = var.ssh_public_key
 
-  diagnostics_storage_account_name      = module.run.logs_storage_account_name
-  azure_monitor_data_collection_rule_id = module.run.data_collection_rule_id
+  diagnostics_storage_account_name = module.run.logs_storage_account_name
+  azure_monitor_data_collection_rule = {
+    id = module.run.data_collection_rule_id
+  }
 
   # Set to null to deactivate backup
-  backup_policy_id = module.run.vm_backup_policy_id
+  backup_policy = {
+    id = module.run.vm_backup_policy_id
+  }
 
   patch_mode                    = "AutomaticByPlatform"
   maintenance_configuration_ids = [module.run.maintenance_configurations["Donald"].id, module.run.maintenance_configurations["Hammer"].id]
 
-  availability_set_id = azurerm_availability_set.vm_avset.id
+  # availability_set_id = azurerm_availability_set.main.id
   # or use Availability Zone
-  # zone_id = 1
+  zone_id = 1
 
   vm_image = {
     publisher = "Debian"
@@ -158,6 +47,10 @@ module "vm" {
     sku       = "10"
     version   = "latest"
   }
+
+  # The feature must be activated upstream:
+  # az feature register --namespace Microsoft.Compute --name EncryptionAtHost --subscription <subscription_id_or_name>
+  encryption_at_host_enabled = true
 
   storage_data_disk_config = {
     appli_data_disk = {
@@ -180,7 +73,7 @@ module "vm" {
     }
   }
 
-  aad_ssh_login_enabled           = true
-  aad_ssh_login_admin_objects_ids = [data.azuread_group.vm_admins_group.object_id]
-  aad_ssh_login_user_objects_ids  = [data.azuread_group.vm_users_group.object_id]
+  entra_ssh_login_enabled           = true
+  entra_ssh_login_admin_objects_ids = [data.azuread_group.vm_admins_group.object_id]
+  entra_ssh_login_user_objects_ids  = [data.azuread_group.vm_users_group.object_id]
 }
--- examples/aadsshlogin/versions.tf
+++ examples/aadsshlogin/versions.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.3"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.0"
+      version = ">= 2.0"
     }
   }
 }
